492d57df2a900b0e3e3939df83376e22dbb1581d
De-Zwaan <46651802+De-Zwaan@users.noreply.github.com>
2021-04-27 17:34:39 +0200

    Added more comments for readbility and fixed bug

--- field.py
+++ field.py
@@ -5,32 +5,39 @@ import random as r
 import cl
 
 class planet:
-    def __init__(self, x, y, mass, radius, surf_color, color):
+    def __init__(self, x, y, mass, radius, surface_color, field_color):
         self.x = x
         self.y = y
         self.m = mass
-        self.surf_color = surf_color
-        self.c = color
         self.r = radius
+        self.surf_color = surface_color
+        self.c = field_color
 
+# A function to go from hsv to rgb, returns a tuple
 def getColor(h, s, v):
     # color = (r.randint(100, 255), r.randint(100, 255), r.randint(100, 255))
     # color = ImageColor.getrgb("hsl(" + str(r.randint(0, 360)) + ", " + str(50) +"%, " + str(75) + "%)")
     return ImageColor.getrgb("hsl(" + str(h) + ", " + str(s) +"%, " + str(v) + "%)")
 
-def generatePlanets(width, height, num_planets, c):
-    # Initialise the planets in an array
-    planets = []
+# Generate some nice looking planets
+def generatePlanets(width, height, num_planets, c, planets):
     i = r.randint(0, 360)
-
+    
+    density = 10
+    
     for n in range(0, num_planets):
-        # Get a nice colour
+        # Get a nice surface colour
         hue = r.randint(i*int(360 / num_planets), (i + 1)*int(360 / num_planets))
         i = i + 1
-        color = c(hue, 70, 70)
-
+        surface_color =     c(hue, 80, 75)
+        field_color =       c(hue, 70, 70)
+        
+        # Generate a mass and a radius
+        mass = r.random()
+        radius = (3 / (4 * math.pi) * mass * density)**(1/3)
+        
         # Add the new planet to the array
-        planets.append(planet(r.randint(0, width), r.randint(0, height), r.random(), color))
+        planets.append(planet(r.randint(0, width), r.randint(0, height), mass, radius, surface_color, field_color))
 
     return planets
 
@@ -53,35 +60,45 @@ def createImage(width, height, planets):
     # Save the image
     img.save('./grav_field.png')
 
+# Dimensions of the final image
 width = 8192
 height = 8192
 
-scale = 128
-r_scale = 1
 
 time = 1
 
-# planets = generatePlanets(width, height, 2, getColor)
-
+# Empty array to store the planets
 planets = []
 
-# sun
-planets.append(planet(width / 2, height / 2,                332900,     109 * r_scale / 5,          getColor(50, 80, 70),   (0, 0, 0)))
-# mercury
-planets.append(planet(width / 2, height / 2 - 0.4 * scale,  0.055,      0.383 * r_scale,        getColor(205, 5, 65),   getColor(200, 10, 60)))
-# venus
-planets.append(planet(width / 2, height / 2 - 0.7 * scale,  0.815,      0.950 * r_scale,        getColor(0, 0, 80),     getColor(0, 0, 70)))
-# earth
-planets.append(planet(width / 2, height / 2 - 1.0 * scale,  1.000,      1.000 * r_scale,        getColor(205, 85, 80),  getColor(205, 80, 70)))
-# mars
-planets.append(planet(width / 2, height / 2 - 1.5 * scale,  0.107,      0.533 * r_scale,        getColor(20, 95, 90),   getColor(25, 90, 80)))
-# jupiter
-planets.append(planet(width / 2, height / 2 - 5.2 * scale,  318.0,      11.209 * r_scale,       getColor(25, 40, 75),   getColor(25, 40, 60)))
-# saturn
-planets.append(planet(width / 2, height / 2 - 9.5 * scale,  95.00,      9.449 * r_scale,        getColor(50, 55, 80),   getColor(55, 40, 70)))
-# uranus
-planets.append(planet(width / 2, height / 2 - 19.2 * scale, 14.00,      4.007 * r_scale,        getColor(180, 50, 95),  getColor(180, 45, 80)))
-# neptune
-planets.append(planet(width / 2, height / 2 - 30.1 * scale, 17.00,      3.883 * r_scale,        getColor(210, 80, 85),  getColor(210, 70, 75)))
+# Randomly create some beautiful planets
+# Comment out if you want to use your own planets
+planets = generatePlanets(width, height, 2, getColor)
 
+# Scales to display the different planets from out solar system at.
+# Scale of the distance to the sun
+d_scale = 128
+# Scale of the planets themselves
+r_scale = 1
+
+# List of a few massive bodies from the solar system
+# sun
+# planets.append(planet(width / 2, height / 2,                332900,     109 * r_scale / 5,          getColor(50, 80, 70),   (0, 0, 0)))
+# mercury
+# planets.append(planet(width / 2, height / 2 - 0.4 * scale,  0.055,      0.383 * r_scale,        getColor(205, 5, 65),   getColor(200, 10, 60)))
+# venus
+# planets.append(planet(width / 2, height / 2 - 0.7 * scale,  0.815,      0.950 * r_scale,        getColor(0, 0, 80),     getColor(0, 0, 70)))
+# earth
+# planets.append(planet(width / 2, height / 2 - 1.0 * scale,  1.000,      1.000 * r_scale,        getColor(205, 85, 80),  getColor(205, 80, 70)))
+# mars
+# planets.append(planet(width / 2, height / 2 - 1.5 * scale,  0.107,      0.533 * r_scale,        getColor(20, 95, 90),   getColor(25, 90, 80)))
+# jupiter
+# planets.append(planet(width / 2, height / 2 - 5.2 * scale,  318.0,      11.209 * r_scale,       getColor(25, 40, 75),   getColor(25, 40, 60)))
+# saturn
+# planets.append(planet(width / 2, height / 2 - 9.5 * scale,  95.00,      9.449 * r_scale,        getColor(50, 55, 80),   getColor(55, 40, 70)))
+# uranus
+# planets.append(planet(width / 2, height / 2 - 19.2 * scale, 14.00,      4.007 * r_scale,        getColor(180, 50, 95),  getColor(180, 45, 80)))
+# neptune
+# planets.append(planet(width / 2, height / 2 - 30.1 * scale, 17.00,      3.883 * r_scale,        getColor(210, 80, 85),  getColor(210, 70, 75)))
+
+# Create the image using the planets in the array
 createImage(width, height, planets)
